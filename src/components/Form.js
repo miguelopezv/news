850aeb9c54c88c7ef67271b8da0281d2425b4434
Miguel López <miguellopez@vitalware.com>
2019-07-30 18:31:05 -0500

    change selected category and retrieve news

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,23 +3,37 @@ import React, { Component } from 'react';
 class Form extends Component {
   state = {
     categories: [
+      'General',
       'Business',
       'Entertainment',
-      'General',
       'Health',
       'Science',
       'Sports',
       'Technology'
-    ]
+    ],
+    selected: 'general'
   };
+
+  handleChange = e => {
+    this.setState(
+      {
+        ...this.state,
+        selected: e.target.value
+      },
+      () => {
+        this.props.getNews(this.state.selected);
+      }
+    );
+  };
+
   render() {
     return (
       <div className="search row">
-        <div className="col s12 m8 offset-2">
+        <div className="col s12 m8 offset-m2">
           <form>
             <h2>Filter by categories</h2>
-            <div className="input-field col s12 m8">
-              <select>
+            <div className="input-field col s12 m8 offset-m2">
+              <select onChange={this.handleChange}>
                 {this.state.categories.map(category => (
                   <option key={category} value={category.toLowerCase()}>
                     {category}
